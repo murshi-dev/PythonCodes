e5869811eb618ced1ccdceabbc9db30588bacffa
murshi <90964789+murshi-dev@users.noreply.github.com>
2024-05-02 10:30:11 +0800

    c
    c

--- PytthonBasics/Chapter4Conditionals/CheckEvenOdd.py
+++ PytthonBasics/Chapter4Conditionals/CheckEvenOdd.py
@@ -1,4 +1,7 @@
-'''Input a number from user. Check and display if the entered number is odd or even number. Using logical operators check if the entered number is positive numbers.'''
+'''Input a number from user. Check and display
+if the entered number is odd or even number.
+Using logical operators check if the entered number
+is positive numbers.'''
 number=int(input("Enter any number: "))
 if(number<0):
     print("Enter positive numbers only")
--- PytthonBasics/Chapter4Conditionals/CheckInput.py
+++ PytthonBasics/Chapter4Conditionals/CheckInput.py
@@ -1,6 +1,8 @@
 #Input a number from user. Check and display if the entered number is a positive or negative number.
 number=int(input("Enter any number: "))
-if(number>0):
+if(number==0):
+    print("Entered number is ZERO")
+elif(number>0):
     print("Entered number is POSITIVE")
 else:
     print("Entered number is NEGATIVE")
