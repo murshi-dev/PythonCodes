73443ed26dda8bf6890df5ac7771ffc5ac13b07b
murshi <90964789+murshi-dev@users.noreply.github.com>
2024-05-24 10:21:38 +0800

    c
    c

--- PythonTuples/TuplesInsideList.py
+++ PythonTuples/TuplesInsideList.py
@@ -1,35 +1,17 @@
-#create a list - add a few tuples in it
-my_list=[(1,'apple'),(2,'orange'),(3,'pears')]
+#create a list with few tuples
+my_list=[(1,'apple'),(2,'berry'),(3,'orange')]
 #display the list
 for data in my_list:
-    print("Number: ",data[0],"Fruit Name: ",data[1])
+    print("Number: ",data[0],"Name: ",data[1])
 
-#get fruit numbers and name from user and it to a list
 #create an empty list
-userinput_fruits=[]
-#add 5 fruits
+userinput_list=[]
+#add 5 tuples
 for i in range(5):
-    fruit_number=int(input("Enter the fruit number: "))
-    fruit_name=input("Enter the fruit name: ")
-    #add these input as a tuple to the list
-    userinput_fruits.append((fruit_number,fruit_name))
-
+    id=int(input("Enter the ID: "))
+    address=input("Enter the address: ")
+    #add/append the input as a tuple to list
+    userinput_list.append((id,address))
 #display the list
-for data in userinput_fruits:
-    print("Fruit Number: ", data[0], "Fruit Name: ", data[1])
-
-
-#try
-#get student id and student names
-#create an empty list
-students_list=[]
-#add 5 students
-for i in range(5):
-    studentId=int(input("Enter the student id: "))
-    studentName=input("Enter the student name: ")
-    #add these input as a tuple to the list
-    students_list.append((studentId,studentName))
-
-#display the list
-for data in students_list:
-    print("Student Id: ", data[0], "Student Name: ", data[1])
+for data in userinput_list:
+    print("ID: ",data[0],"Address: ",data[1])
