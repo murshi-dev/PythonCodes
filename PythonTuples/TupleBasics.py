986f1fd9e77fe7427afcadeca77d7a243c5c1caf
murshi <90964789+murshi-dev@users.noreply.github.com>
2024-05-24 10:00:32 +0800

    c
    c

--- PythonTuples/TupleBasics.py
+++ PythonTuples/TupleBasics.py
@@ -1,25 +1,31 @@
 #tuple is a data type in python
 #used to store a collection of items
-#tuples are not chnageable - immutable
+#tuples are not changeable - immutable
+
+#create a tuple
 sample_tuple=(1,2,'hi')
+#print all the contents of the tuple
 print(sample_tuple)
-print("First element:", sample_tuple[0])
-print("Second element:", sample_tuple[1])
-print("Third element:", sample_tuple[2])
+#print each element using index
+print("First element: ",sample_tuple[0])
+print("Second element: ",sample_tuple[1])
+print("Third element: ",sample_tuple[2])
 #print last element
-print("Last element:", sample_tuple[-1])
-#modify an elemnt
-# sample_tuple[0]=53 - contents cannot be changed
-#display elemnts in tuple using for loop
+print("Last element: ",sample_tuple[-1])
+#try to change an element
+#sample_tuple[0]=23 --raises an error
+
+#display elements in tuple using for loop
 for item in sample_tuple:
-    print("Data: ",item)
+    print("Elements: ",item)
 
-#find the length of tuple
-print("Number of elements in tuple is",len(sample_tuple))
+#find the length of the tuple
+print("Number of elements: ",len(sample_tuple))
 
-#tuples can concatenate - add
+#tuples can be joined together - concatenate
 tuple1=(1,2,3)
 tuple2=('a','b','c')
-combined_tuples=tuple1+tuple2
+combined_tuples = tuple1 + tuple2
+#print
 for data in combined_tuples:
     print(data)
